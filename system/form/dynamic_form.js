5722cc4e78091352e2e48371723a9ea7e960fb09
Paulo Henrique Lisboa <phlinho10@gmail.com>
2019-05-10 17:13:48 -0300

    terminar em casa

--- system/form/dynamic_form.js
+++ system/form/dynamic_form.js
@@ -1,24 +1,45 @@
 $(document).ready(function(){
-    var next = 1;
-    $(".add-more").click(function(e){
-        e.preventDefault();
-        var addto = "#field" + next;
-        var addRemove = "#field" + (next);
-        next += 1;
-        var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
-        var newInput = $(newIn);
-        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
-        var removeButton = $(removeBtn);
-        $(addto).after(newInput);
-        $(addRemove).after(removeButton);
-        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-        $("#count").val(next);  
-            $('.remove-me').click(function(e){
-                e.preventDefault();
-                var fieldNum = this.id.charAt(this.id.length-1);
-                var fieldID = "#field" + fieldNum;
-                $(this).remove();
-                $(fieldID).remove();
-            });
-    });
+    function addNewChoiceInput() {
+        $ret = '';
+        $ret += "<div class='form-group has-warning has-feedback'>";
+            $ret += "<label class='col-sm-2 control-label'>Tipo</label>";
+            $ret += "<div class='col-sm-4'>";
+                $ret += "<select id='s2_with_tag' multiple='multiple' class='populate placeholder'>";
+                    $ret += "<option>Checkbox</option>";
+                    $ret += "<option>Color</option>";
+                    $ret += "<option>Date</option>";
+                    $ret += "<option>Date-Time</option>";
+                    $ret += "<option>Email</option>";
+                    $ret += "<option>Number</option>";
+                    $ret += "<option>Radio</option>";
+                    $ret += "<option>Range</option>";
+                    $ret += "<option>Tel</option>";
+                    $ret += "<option>text</option>";
+                    $ret += "<option>Time</option>";
+                    $ret += "<option>Url</option>";
+                    $ret += "<option>Week</option>";
+                $ret += "</select>";
+            $ret += "</div>";
+        $ret += "</div>";
+        return $ret
+    }
+
+    function addNewInput(this) {
+        
+    }
+
+
+
+    <div class="form-group has-warning has-feedback">
+        <label class="col-sm-2 control-label">Estado civil</label>
+        <div class="col-sm-4">
+            <select id="s2_with_tag" multiple="multiple" class="populate placeholder">
+                <option>Solteiro</option>
+                <option>Casado</option>
+                <option>Separado</option>
+                <option>Divorciado</option>
+                <option>Viúvo</option>
+            </select>
+        </div>
+    </div>
 });
