f3bca78ed9064a9431f777970e3cc56dced46545
Paulo Henrique Lisboa <phlinho10@gmail.com>
2019-08-16 17:40:33 -0300

    tudo com erro UHEHUEH

--- src/components/masterpage/adminPanel/header/index.js
+++ src/components/masterpage/adminPanel/header/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import { Container } from './styles';
+import bulma from 'bulma';
 
 export default function Header() {
   return (
-    <Container>
-      <h1>Rocketfy</h1>
-    </Container>
-  );
-}
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="112" height="28">
+    </a>
+
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+  </div>
+</nav>
+      );
+    }
--- src/components/masterpage/adminPanel/header/styles.js
+++ src/components/masterpage/adminPanel/header/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 export const Container = styled.div`
   height: 80px;
   padding: 0 30px;
