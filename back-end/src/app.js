722e246d84580e3503da2d6e876a5f90a6a144d4
Paulo Henrique Lisboa <phlinho10@gmail.com>
2019-10-11 17:58:13 -0300

    haha

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,21 @@
-
-const {connect} = require('./controller/db/connectDB');
+require('dotenv').config();
+const {connect}     = require('./controller/db/connectDB');
 const express       = require('express');
 const app           = express();
 const bodyParser    = require('body-parser');
+const cors          = require('cors');
+const routes        = require('./routes');
 
-connect();
+app.use(cors());
+
+//Connect Database
+// connect();
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.SERVER_PORT || 3333);
